4afcfcab4ce57506a51078a480d36350369b3306
Ersultan Mansur <nullmansurov@gmail.com>
2024-09-12 19:09:47 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+    (function() {
+        var d = document, s = d.createElement('script');
+        s.src = 'https://scaload-1.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+
+
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/5d760a41eb1a6b0be608b5e8/default';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
